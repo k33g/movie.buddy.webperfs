7865ff333f22013b7821e853784ecefe46d089cf
k33g <ph.charriere@gmail.com>
2014-04-27 20:21:50 +0200

    tests

---
+++ tests/README.md
@@ -0,0 +1,12 @@
+gatling-maven-plugin-demo
+=========================
+
+Simple showcase of a maven project using the gatling-maven-plugin.
+
+To test it out, simply execute the following command :
+
+    $mvn gatling:execute -Dgatling.simulationClass=basic.BasicExampleSimulation
+
+    $mvn gatling:execute -Dgatling.simulationClass=org.k33g.moviebuddy.tests.AllMoviesLoadingScenario
+
+    $mvn gatling:execute -Dgatling.simulationClass=org.k33g.moviebuddy.tests.SomeMoviesLoadingScenario
---
+++ tests/src/test/scala/org/k33g/moviebuddy/tests/AllMoviesLoadingScenario.scala
@@ -0,0 +1,32 @@
+package org.k33g.moviebuddy.tests
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+import io.gatling.http.Headers.Names._
+import io.gatling.http.request._
+import scala.concurrent.duration._
+import bootstrap._
+import assertions._
+
+
+class AllMoviesLoadingScenario extends Simulation {
+  val title = System.getProperty("title", "localhost")
+  val server = System.getProperty("buddyserver", "http://192.168.128.142:3000");
+  //val server = System.getProperty("buddyserver", "http://localhost:3000");
+  val totalUsers = toInt(System.getProperty("gatling.users", "100"));
+  val loops = toInt(System.getProperty("gatling.loops", "100"));
+  val scn = scenario("Loading all movies (" + totalUsers + " users/" + loops + " loops)").repeat(loops) {
+    exec(
+      http("Loading all movies")
+        .get(server + "/movies")
+        .check(status.is(200)))
+  }
+
+  setUp(scn
+    .inject(ramp(totalUsers users) over (totalUsers seconds)))
+}
+
+/*
+    mvn gatling:execute -Dgatling.simulationClass=org.k33g.moviebuddy.tests.AllMoviesLoadingScenario
+ */
